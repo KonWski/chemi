b0252f77d1e340718edd98218861006c88d29119
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2026-02-14 22:39:33 +0100

    randomsplitter molecule_ids fix

--- chemi/cv_splitters/random_splitter.py
+++ chemi/cv_splitters/random_splitter.py
@@ -7,14 +7,14 @@ class RandomSplitter(Splitter):
         super().__init__(cv)
         self.seed = seed
 
-    def split(self, smiles, labels):
+    def split(self, molecule_ids, smiles, labels):
+
+        n_obs = len(smiles)
+        n_obs_cv = int(n_obs / self.cv)
 
         random.seed(self.seed)
-        molecules_id = [molecule_id for molecule_id in range(len(smiles))]
-        shuffled_molecules_id = random.shuffle(molecules_id)
-
-        n_obs = len(molecules_id)
-        n_obs_cv = int(n_obs / self.cv)
+        internal_molecule_ids = [id for id in range(n_obs)]
+        shuffled_internal_molecules_id = random.shuffle(internal_molecule_ids)
 
         # folds as fold_id, smiles, labels, original_id
         folds = {fold_id: {"smiles": [], "labels": [], "molecules_id": []} for fold_id in range(self.cv)}
@@ -23,10 +23,10 @@ class RandomSplitter(Splitter):
 
             index_start = fold_id * n_obs_cv
             index_end = (fold_id + 1) * n_obs_cv
-            select_molecules_id = shuffled_molecules_id[index_start: index_end]
+            select_molecules_id = shuffled_internal_molecules_id[index_start: index_end]
 
             folds[fold_id]["smiles"] = [smiles[molecule_id] for molecule_id in select_molecules_id]
             folds[fold_id]["labels"] = [labels[molecule_id] for molecule_id in select_molecules_id]
-            folds[fold_id]["molecules_id"] = select_molecules_id
+            folds[fold_id]["molecules_id"] = [molecule_ids[molecule_id] for molecule_id in select_molecules_id]
 
         self.folds = folds
